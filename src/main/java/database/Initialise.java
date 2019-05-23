04ccef49e9fa965e3c600aca33256a1308e6b2ad
TGilbert <Thomas.Gilbert@academytrainee.com>
2019-05-23 16:29:17 +0100

    added notes on how to use class Initialise

--- src/main/java/database/Initialise.java
+++ src/main/java/database/Initialise.java
@@ -1,8 +1,10 @@
 package database;
 
 public final class Initialise {
-
-	public String createUserTable() {
+	//stores methods that return valid statements to create tables in a database, these methods should only be called if the table they
+	//create do not already exist (use an Optional statement?)
+	
+	public String createUserTable() {//may not be necessary
 		String returnValue = "create table user (uid int, name varchar(50), primary key(uid))";
 		return returnValue;
 	}
@@ -12,7 +14,5 @@ public final class Initialise {
 				+ "ice_res int, dragon_res int, primary key(uid))";
 		return returnValue;
 	}
-	
-	
 	
 }
