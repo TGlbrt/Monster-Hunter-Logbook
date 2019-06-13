c60dc2e4fd8d48de9b4c5660c9eff875de41d561
TGilbert <thomasgilbert.uk@gmail.com>
2019-06-13 23:31:22 +0100

    removed incorrect imports in LoggerEndpoints

--- src/main/java/rest/LoggerEndpoints.java
+++ src/main/java/rest/LoggerEndpoints.java
@@ -1,24 +1,24 @@
 package rest;
 
-import javax.ws.rs.Path;
-
-import java.net.URI;
 import java.util.List;
 
+import java.net.URI;
+
 import javax.inject.Inject;
-import javax.websocket.server.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
 
 import logger.Log;
 import logger.LoggerRepository;
