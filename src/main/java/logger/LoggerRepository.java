bfad35add4990b013df679d1fc825b5d11194e49
TGlbrt <48251662+TGlbrt@users.noreply.github.com>
2019-06-14 08:40:49 +0100

    Revert "Bck log"

--- src/main/java/logger/LoggerRepository.java
+++ src/main/java/logger/LoggerRepository.java
@@ -1,20 +1,5 @@
 package logger;
 
-import java.util.List;
-
 public interface LoggerRepository {
 
-	//create
-	public Log createLog(Log log);
-	//read one
-	public Log getLog(int id);
-	//read many
-	public List<Log> getAllLogs();
-	public List<Log> getAllUserLogs(String username);
-	public List<Log> getUserLogsByMonster(String userName,String monsterName);
-	//update
-	public Log changeExistingLog(int id, Log log);
-	//delete
-	public void removeLog(int id);
-	
 }
---
+++ src/main/resources/import_sql.sql
@@ -0,0 +1,2 @@
+insert into Account(id,name,value) values (1,'thom', 17.48);
+insert into Account(id,name,value) values (2,'ghom', 124.12);
