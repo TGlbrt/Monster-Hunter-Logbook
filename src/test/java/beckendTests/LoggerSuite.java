cbf7a314e68e8ee78f795e64d49c52b673515acf
TGilbert <thomasgilbert.uk@gmail.com>
2019-06-16 13:28:06 +0100

    added entity test classes to the test suite

--- src/test/java/beckendTests/LoggerSuite.java
+++ src/test/java/beckendTests/LoggerSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({UserTests.class})
+@SuiteClasses({UserEntityTests.class,MonsterEntityTests.class,LogEntityTests.class})
 public class LoggerSuite {
 
 }
