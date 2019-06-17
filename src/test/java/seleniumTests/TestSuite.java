282d403463950e2fa1251a363eb54783ec94bb13
TGilbert <thomasgilbert.uk@gmail.com>
2019-06-17 14:30:08 +0100

    added Back-end JUnit tests to run with the selenium TestSuite class

--- src/test/java/seleniumTests/TestSuite.java
+++ src/test/java/seleniumTests/TestSuite.java
@@ -5,10 +5,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import beckendTests.LoggerSuite;
 import myCategories.*;
 
 @RunWith(Suite.class)
-@SuiteClasses({IndexTests.class,LoginTests.class,MonstersTests.class,LogTests.class})
+@SuiteClasses({IndexTests.class,LoginTests.class,MonstersTests.class,LogTests.class,LoggerSuite.class})
 @Categories.IncludeCategory({Post.class, Put.class, Get.class,Delete.class,NonCRUD.class})
 public class TestSuite {
 
